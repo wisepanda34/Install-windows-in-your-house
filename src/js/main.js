427d689cd700d7523a05dbc12105dbdcae649ad2
evghen.naydenko@gmail.com <evghen.naydenko@gmail.com>
2023-01-23 19:02:10 +0200

    added first modal and first tab

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// import './slider';
+// import { moduls } from './modules/modals.js';
+
+"use strict";
+console.log('1');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	moduls();
+	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,42 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+
+	const header = document.querySelector(headerSelector);
+	const tab = document.querySelectorAll(tabSelector);
+	const content = document.querySelectorAll(contentSelector);
+
+	function hideTabContent() {
+		content.forEach(item => {
+			item.style.display = 'none';
+		})
+		tab.forEach(item => {
+			item.classList.remove(activeClass);
+		})
+	}
+	function showTabContent(i = 0) {
+		content[i].style.display = 'flex';
+		tab[i].classList.add(activeClass);
+		console.log('i=' + i);
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	//для отслеживания на какой таб кликает пользователь используем делегирование событий
+	header.addEventListener('click', (e) => {
+		const target = e.target;
+		//проверка на клик по нужному селектору(не по пробелам между вкладками), или если кликнет по ребенку
+		//для игнорирования точки используем regular expration
+		//
+		if (target &&
+			(target.classList.contains(tabSelector.replace(/\./, "")) ||
+				target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+			tab.forEach((item, i) => {
+				if (target == item || target.parentNode == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+}; 
